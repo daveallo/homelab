ea6849b701562cdfe339ce7e48d4d573d1da5f49
daveallo <44402983+daveallo@users.noreply.github.com>
2025-11-10 22:38:58 +0100

    Fix file paths and quotes in acme.sh

--- g1-winads-1/acme.sh
+++ g1-winads-1/acme.sh
@@ -8,14 +8,14 @@ export CF_Token=$ACME_TOKEN && acme.sh \
   --dns dns_cf \
   --dnssleep 30 \
   --keylength 2048 \
-  --cert-file '$HOME/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.crt' \
-  --key-file '$HOME/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.key' \
-  --pre-hook 'mkdir -p $HOME/.ssl' \
+  --cert-file $HOME/.ssl/$HOSTNAME.$DOMAIN_NAME.crt \
+  --key-file $HOME/.ssl/$HOSTNAME.$DOMAIN_NAME.key \
+  --pre-hook 'mkdir -p '$HOME'/.ssl' \
   --reloadcmd 'openssl pkcs12 \
   -export \
-  -out $HOME/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.pfx \
-  -in $HOME/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.crt \
-  -inkey $HOME/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.key \
+  -out '$HOME'/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.pfx \
+  -in '$HOME'/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.crt \
+  -inkey '$HOME'/.ssl/'$HOSTNAME'.'$DOMAIN_NAME'.key \
   -password pass:'$PFX_PASSWORD' && \
     powershell "Get-ChildItem -Path Cert:\\LocalMachine\\My | Remove-Item" && \
     powershell "Get-WmiObject -Class Win32_TSGeneralSetting -Namespace root\\cimv2\\terminalservices | \`
