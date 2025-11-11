7b790711b433f30106192fdb947cfe451661571c
daveallo <44402983+daveallo@users.noreply.github.com>
2025-11-11 14:57:34 +0100

    Add PowerShell script for domain joining

---
+++ winsrv/domain-join.ps1
@@ -0,0 +1,9 @@
+Add-Computer `
+  -DomainName $env:DOMAIN_NAME `
+  -Credential $(New-Object `
+    -TypeName PSCredential `
+    -ArgumentList ([pscustomobject]@{
+      UserName = "Administrator";
+      Password = (ConvertTo-SecureString -String $env:AD_PASSWORD -AsPlainText -Force)[0]})) `
+  -OUPath $env:AD_PATH `
+  -Restart
