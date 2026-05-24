261b72270f4115dacbcb466230d65849c0206dda
daveallo <44402983+daveallo@users.noreply.github.com>
2026-05-24 12:16:06 +0200

    Update domain join script to use NETBIOS format

--- windows/domain-join.ps1
+++ windows/domain-join.ps1
@@ -3,7 +3,7 @@ Add-Computer `
   -Credential (New-Object `
     -TypeName PSCredential `
     -ArgumentList ([pscustomobject]@{
-      UserName = $env:AD_USER;
+      UserName = $env:DOMAIN_NETBIOS+"\"+$env:AD_USER;
       Password = (ConvertTo-SecureString -String $env:AD_PASSWORD -AsPlainText -Force)[0]})) `
   -OUPath $env:AD_OU_PATH `
   -Restart
