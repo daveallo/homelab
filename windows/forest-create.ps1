7354f49ee8d62c748c8a6af6f70d92de5d7ece22
daveallo <44402983+daveallo@users.noreply.github.com>
2026-05-24 11:51:03 +0200

    Add PowerShell script to create AD DS forest

---
+++ windows/forest-create.ps1
@@ -0,0 +1,10 @@
+Install-ADDSForest `
+  -DomainName $env:DOMAIN_NAME `
+  -SafeModeAdministratorPassword $(ConvertTo-SecureString -String $env:AD_SMA_PASSWORD -AsPlainText -Force) `
+  -CreateDnsDelegation:$false `
+  -DomainMode "WinThreshold" `
+  -DomainNetbiosName $env:DOMAIN_NETBIOS `
+  -ForestMode "WinThreshold" `
+  -InstallDns:$true `
+  -NoRebootOnCompletion:$false `
+  -Force:$true
